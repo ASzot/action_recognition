f3e61bdf7251e11f3279a58304c70026b908266c
Andrew Szot <me@andrewszot.com>
2018-11-12 20:40:51 -0800

    Updated code with Michelle

--- dataset.py
+++ dataset.py
@@ -12,6 +12,18 @@ class MomentsDataset(Dataset):
     def __init__(self, data_path):
         self.data_path = data_path
         self.df = pd.read_csv(self.data_path + 'trainingSet_filtered.csv')
+        self.class_to_index = {}
+        actions = [file_name.split('/')[0] for file_name in
+                list(self.df[self.df.columns[1]])]
+
+        actions = set(actions)
+
+        for i, action in enumerate(actions):
+            self.class_to_index[action] = i
+
+
+    def get_num_classes(self):
+        return len(self.class_to_index)
 
     def __len__(self):
         return len(self.df)
@@ -27,21 +39,24 @@ class MomentsDataset(Dataset):
         for image in images:
             im = cv2.imread(osp.join(dir_path, image))
             resized_image = cv2.resize(im, (128, 128))
-            gray_im = rgb2gray(resized_image)
+            #gray_im = rgb2gray(resized_image)
 
-            im_data.append(gray_im)
+            im_data.append(resized_image)
 
         im_data = np.array(im_data)
+        im_data = im_data / 255.0
         im_data = im_data[:90]
         if len(im_data) < 90:
-            paste_im_data = np.zeros((90, 128, 128))
+            paste_im_data = np.zeros((90, 128, 128, 3))
             paste_im_data[:len(im_data)] = im_data
             paste_im_data[len(im_data):] = im_data[-1]
             im_data = paste_im_data
 
+        index = self.class_to_index[label]
+
         ret_info = {
-                'images': im_data,
-                'label': label
+                'images': im_data[0],
+                'label': index
                 }
 
         return ret_info
--- main.py
+++ main.py
@@ -1,15 +1,71 @@
 from dataset import MomentsDataset
 from torch.utils.data import Dataset, DataLoader
 
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+
+class CnnNet(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 29 * 29, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 29 * 29)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
 
 dataset_path = '/hdd/datasets/moments/Moments_in_Time_256x256_30fps/'
 
 ds = MomentsDataset(dataset_path)
 
-dataloader = DataLoader(ds, batch_size=4, shuffle=True, num_workers=1)
+#label_count = ds.get_num_classes()
+batch_size = 4
 
-for batch in dataloader:
-    print(batch['images'].shape)
-    print(batch['label'])
+dataloader = DataLoader(ds, batch_size=batch_size, shuffle=True, num_workers=1)
+
+model = CnnNet(ds.get_num_classes())
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+N_EPOCHS = 10
+
+for epoch_i in range(N_EPOCHS):
+    for batch in dataloader:
+
+        X = batch['images']
+        labels = batch['label']
+
+        #X = X.reshape(batch_size, 3, 128, 128, 90)
+        X = X.reshape(batch_size, 3, 128, 128)
+        X = X.float()
+        predicted_labels = model(X)
+
+        labels = labels.long()
+
+        loss = criterion(predicted_labels, labels)
+
+        loss.backward()
+        optimizer.step()
+
+        print('Loss: %.5f' % (loss.data.cpu().numpy()))
+
+
 
 
